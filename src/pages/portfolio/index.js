0e550ea314fddb6465d94a3c6161552ed200357d
Avisenna Abimanyu <theknightmare07@gmail.com>
2024-10-09 22:00:55 +0700

    add portfolio

--- src/pages/portfolio/index.js
+++ src/pages/portfolio/index.js
@@ -1,3 +1,4 @@
+import Title from "@/components/atoms/Title";
 import Layout from "@/components/templates/Layout";
 import React from "react";
 
@@ -5,7 +6,19 @@ const PortfolioPage = () => {
   return (
     <>
       <Layout>
-        <div>portfolio</div>
+        <div className="pt-[40px]">
+          <Title className="text-center" color="text-black">
+            Portfolio
+          </Title>
+          <div className="flex justify-center my-[40px]">
+            <iframe
+              src="https://docs.google.com/document/d/1mSmqjZFBDHW9dXd-CGfu9odwFOwu2FFup7s_YSsaiHU/preview"
+              width="700"
+              height="600"
+              allowfullscreen=""
+            ></iframe>
+          </div>
+        </div>
       </Layout>
     </>
   );
