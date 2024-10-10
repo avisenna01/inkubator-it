259b6ab3eeff1dee8ae4806ce78b96bf4bb8651a
Avisenna Abimanyu <theknightmare07@gmail.com>
2024-10-10 09:17:23 +0700

    add input

--- src/pages/ajukanProyek/index.js
+++ src/pages/ajukanProyek/index.js
@@ -1,3 +1,4 @@
+import Input from "@/components/atoms/Input";
 import Title from "@/components/atoms/Title";
 import Layout from "@/components/templates/Layout";
 import React from "react";
@@ -8,6 +9,10 @@ const AjukanProyekPage = () => {
       <Layout>
         <div className="p-[80px]">
           <Title color="text-black">Formulir Pengajuan Proyek</Title>
+          <p>Sudah siap untuk mengajukan proyek?</p>
+          <p>Yuk, isi formulir di bawah ini!</p>
+          <p>*) Wajib diisi</p>
+          <Input name="Nama" label="Nama Lengkap" placeholder="John doe" />
         </div>
       </Layout>
     </>
